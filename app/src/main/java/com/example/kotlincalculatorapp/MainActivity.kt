b85b7dbfdf6820ce846765f80f8b8719972b466c
kwonjuyoung <20161527@vision.hoseo.edu>
2022-03-18 00:44:33 +0900

    계산기 UI 완성

--- app/src/main/java/com/example/kotlincalculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlincalculatorapp/MainActivity.kt
@@ -2,13 +2,24 @@ package com.example.kotlincalculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var Input: TextView ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Input = findViewById(R.id.Input)
 
 
+    }
+
+    fun onDigit(view: View){
+        Input?.append((view as Button).text)
 
     }
 }
